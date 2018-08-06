a14b6ba0826b320c39d9ff78a0c99399f96b920b
Charlie Dalton <charliedalton@oneanymousboi.hsd1.ca.comcast.net>
2018-08-06 14:58:35 -0700

    Add form for messages and show incoming messages in browser

--- public/js/index.js
+++ public/js/index.js
@@ -11,4 +11,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
   console.log('newMessage', message);
+  var link = jQuery('<li></li>');
+  link.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(link);
+});
+
+jQuery('#message-form').on('submit', function(e) {
+  e.preventDefault();
+
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  });
 });
